e55124b79753a06b7441dd6898899235462e35f1
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2022-05-01 09:34:09 +0530

    Create Total Appeal of A String.cpp

---
+++ Contests/Weekly Contest 291/Programs/Total Appeal of A String.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    long long appealSum(string S) 
+    {
+        const int NO_OF_ALPHABETS = 26;
+        long long answer = 0;
+        vector <int> first_index(NO_OF_ALPHABETS, NOT_FOUND);    
+        for(int i = 0; i < S.size(); i++)
+        {   
+            if(i == 0)
+            {
+                contribution[i] = 1;
+            }
+            else if(S[i] == S[i - 1])
+            {
+                contribution[i] = contribution[i - 1] + 1;
+            }
+            else 
+            {
+                if(first_index[S[i] - 'a'] == NOT_FOUND)
+                {
+                    contribution[i] = contribution[i - 1] + i + 1;
+                }
+                else 
+                {
+                    contribution[i] = contribution[i - 1] + (i - first_index[S[i] - 'a']) + 1;
+                }
+            }
+            
+            first_index[S[i] - 'a'] = i + 1;
+            
+            answer += contribution[i];
+        }
+        
+        return answer;
+    }
+};
