1346df475ddedb1a9debca75e2f4255620626ed4
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2023-05-28 14:14:22 +0530

    Update Minimum Cost to Make All Characters Equal.cpp

--- Contests/Weekly Contest 347/Programs/Minimum Cost to Make All Characters Equal.cpp
+++ Contests/Weekly Contest 347/Programs/Minimum Cost to Make All Characters Equal.cpp
@@ -2,13 +2,6 @@ class Solution {
 public:
     long long minimumCost(string S) {
         
-        vector <int> last_different(S.size());
-        last_different[0] = -1;
-        for(int i = 1; i < S.size(); i++)
-        {
-            last_different[i] = (S[i] != S[i - 1] ? i - 1: last_different[i - 1]);
-        }
-        
         vector <vector <long long> > prefix_cost(S.size(), vector <long long> (2, 0));
         for(int i = 0; i < S.size(); i++)
         {
@@ -20,21 +13,9 @@ public:
                 }
                 else 
                 {
-                    prefix_cost[i][bit] = (i + 1);
-                    
-                    if(last_different[i] != -1)
-                    {
-                        prefix_cost[i][bit] += prefix_cost[last_different[i]][bit^1];
-                    }
+                    prefix_cost[i][bit] = (i + 1) + (i == 0 ? 0 : prefix_cost[i - 1][bit^1]);
                 }
             }
-        }
-        
-        vector <int> next_different(S.size());
-        next_different[S.size() - 1] = S.size(); 
-        for(int i = S.size() - 2; i >= 0; i--)
-        {
-            next_different[i] = (S[i] != S[i + 1] ? i + 1 : next_different[i + 1]);
         }
         
         vector <vector <long long> > suffix_cost(S.size(), vector <long long> (2, 0));
@@ -48,12 +29,7 @@ public:
                 }
                 else 
                 {
-                    suffix_cost[i][bit] = (S.size() - i);
-                    
-                    if(next_different[i] != S.size())
-                    {
-                        suffix_cost[i][bit] += suffix_cost[next_different[i]][bit^1];
-                    }
+                    suffix_cost[i][bit] = (S.size() - i) + (i == S.size() - 1 ? 0 : suffix_cost[i + 1][bit^1]);
                 }
             }
         }
