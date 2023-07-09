746ef89eed72660edd38526e8733b8c405860995
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2023-07-09 09:51:26 +0530

    Update Maximum Number of Jumps to Reach the Last Index.cpp

--- Contests/Weekly Contest 353/Programs/Maximum Number of Jumps to Reach the Last Index.cpp
+++ Contests/Weekly Contest 353/Programs/Maximum Number of Jumps to Reach the Last Index.cpp
@@ -6,20 +6,20 @@ class Solution {
 public:
     int maximumJumps(vector<int>& nums, int target) {
         int no_of_elements = nums.size();
-        const int oo = 1e9;
-        vector <int> max_jumps_from(no_of_elements, -oo);
+        const int UNREACHABLE = -1;
+        vector <int> max_jumps_from(no_of_elements, UNREACHABLE);
         max_jumps_from[no_of_elements - 1] = 0;
         for(int i = no_of_elements - 2; i >= 0; i--)
         {
             for(int j = i + 1; j <= no_of_elements - 1; j++)
             {
-                if(-target <= nums[j] - nums[i] && nums[j] - nums[i] <= target && max_jumps_from[j] != -oo)
+                if(-target <= nums[j] - nums[i] && nums[j] - nums[i] <= target && max_jumps_from[j] != UNREACHABLE)
                 {
                     max_jumps_from[i] = max(max_jumps_from[i], 1 + max_jumps_from[j]);
                 }
             }
         }
-
-        return (max_jumps_from[0] == -oo ? -1 : max_jumps_from[0]);
+        
+        return max_jumps_from[0];
     }
 };
